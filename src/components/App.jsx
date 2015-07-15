f65097b945361df5cb1441dc01b86391bdc56b47
Ruben Barilani <ruben.barilani@zalando.de>
2015-07-15 17:56:47 +0200

    refactor scss organization, include react-bootstrap, include sass-boostrap styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Stats from './stats/Stats.jsx';
+import {Button} from 'react-bootstrap';
 
 class App extends React.Component{
   render(){
     return (
       <div className="main-container">
         <Stats/>
+        <Button className="join-us-btn" bsStyle="primary" bsSize="large">Join us</Button>
       </div>
     );
   }
