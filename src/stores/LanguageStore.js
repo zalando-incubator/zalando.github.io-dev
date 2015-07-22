a820da6cb079edb233e802391d65ac8e63537bc6
Angela Igreja <angel.igreja@gmail.com>
2015-07-22 16:28:35 +0200

    Add filter buttons only for the top 5 programming languages. Add button 'other' that filters all non top programming languages.

--- src/stores/LanguageStore.js
+++ src/stores/LanguageStore.js
@@ -3,12 +3,21 @@ import AppDispatcher from '../dispatchers/AppDispatcher.js';
 import AppConstants from '../constants/AppConstants.jsx';
 import languagesUtil from '../utils/LanguagesUtil.js';
 import BaseStoreMixin from './BaseStoreMixin.js';
+import _ from 'lodash';
+
+//top languages are the top five most used
+let TOP_LANGUAGES_COUNT = 5;
 
 let languages = [];
+let topLanguages = [];
 
 let LanguageStore = assign({}, BaseStoreMixin, {
   getLanguages: function() {
     return languages;
+  },
+
+  getTopLanguages: function() {
+    return topLanguages;
   }
 });
 
@@ -16,6 +25,8 @@ LanguageStore.dispatchToken = AppDispatcher.register(function(action) {
   switch (action.type) {
     case AppConstants.ActionTypes.RECEIVE_REPOS:
       languages = languagesUtil.guessLanguages(action.repos);
+      //languages is ordered by popularity
+      topLanguages = _.take(languages, TOP_LANGUAGES_COUNT);
       LanguageStore.emitChange();
       break;
 
