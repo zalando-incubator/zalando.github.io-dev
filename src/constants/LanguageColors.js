2263ec8f5f60e0bdc7b46d3b60948f25e0a71488
Angela Igreja <angel.igreja@gmail.com>
2015-07-29 15:02:44 +0200

    Add file with the colors.

--- src/constants/LanguageColors.js
+++ src/constants/LanguageColors.js
@@ -1,6 +1,6 @@
 import GITHUBCOLORS from '../config/github-colors.json';
 
 const LANGUAGE_COLORS = GITHUBCOLORS;
-LANGUAGE_COLORS.DEFAULT= '#B3B3B3';
+LANGUAGE_COLORS.DEFAULT = '#B3B3B3';
 
 export default LANGUAGE_COLORS;
