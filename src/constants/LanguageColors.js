aa6a318f3258a8720ec83eb3a75d6a0bef93af98
Angela Igreja <angel.igreja@gmail.com>
2015-07-29 17:30:14 +0200

    Some color refactoring.

--- src/constants/LanguageColors.js
+++ src/constants/LanguageColors.js
@@ -2,5 +2,5 @@ import GITHUBCOLORS from '../config/github-colors.json';
 
 const LANGUAGE_COLORS = GITHUBCOLORS;
 LANGUAGE_COLORS.DEFAULT = '#B3B3B3';
-
+LANGUAGE_COLORS.JavaScript = '#f4cf34';
 export default LANGUAGE_COLORS;
