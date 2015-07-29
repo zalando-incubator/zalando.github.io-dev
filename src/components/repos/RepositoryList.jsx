54233ef24fe0116d144adfd902efff8470017af5
Angela Igreja <angel.igreja@gmail.com>
2015-07-29 17:49:18 +0200

    Truncate the header of the repository list item. Remove animations.

--- src/components/repos/RepositoryList.jsx
+++ src/components/repos/RepositoryList.jsx
@@ -2,24 +2,18 @@
  * Created by aigreja on 15/07/15.
  */
 
-import React from 'react/addons';
+import React from 'react';
 import RepositoryListItem from './RepositoryListItem.jsx';
 import {Row} from 'react-bootstrap';
 
 class RepositoryList extends React.Component {
 
-
-
   render() {
-    var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
     return (
         <Row className='show-grid'>
-          <ReactCSSTransitionGroup transitionName="example" transitionAppear={true}>
           {this.props.repositories.map(function(repo, i){
-            console.log(repo);
             return <RepositoryListItem key={i} data={repo} />;
           })}
-          </ReactCSSTransitionGroup>
         </Row>
     );
   }
