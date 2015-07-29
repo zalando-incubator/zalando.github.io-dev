9d0107c3ee11a32bfec01bf30117285d7ed4fa8b
Angela Igreja <angel.igreja@gmail.com>
2015-07-29 14:42:26 +0200

    Add organization name in the repository name.

--- src/components/repos/RepositoryListItem.jsx
+++ src/components/repos/RepositoryListItem.jsx
@@ -13,11 +13,11 @@ class RepositoryListItem extends React.Component {
       borderLeft: '3px solid ' + ( languagesUtils.getLanguageColor(this.props.data.primaryLanguage) )
     };
     let primaryLanguage = this.props.data.primaryLanguage ? this.props.data.primaryLanguage : 'unknown';
-
+    const repoName = (<span> {this.props.data.organizationName} / <b>{this.props.data.name}</b> </span>);
     return (
       <Col xs={12} sm={6} md={4}>
         <a href={RepositoryListItem.buildRepoUrl(this.props.data)} target="_blank" className="repo-item-box">
-          <Panel header={this.props.data.name} style={colorBar}>
+          <Panel header={repoName} style={colorBar}>
             <p className="repo-item-description">{this.props.data.description}</p>
             <Row className='show-grid text-center'>
               <Col xs={4} md={4}>
