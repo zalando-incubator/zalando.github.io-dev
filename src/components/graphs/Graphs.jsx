39415ef9e712f187dd5b96aecdbb81148c196288
Mikio Braun <mikiobraun@gmail.com>
2015-08-20 17:03:42 +0200

    Added Graph section stub
    more to come soon... ;)

---
+++ src/components/graphs/Graphs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SectionHeading from '../section-heading/SectionHeading.jsx';
+
+class Graphs extends React.Component {
+  render() {
+    return (
+      <div className='container section'>
+        <SectionHeading text="graphs" />
+      </div>
+    );
+  }
+}
+
+export default Graphs;
