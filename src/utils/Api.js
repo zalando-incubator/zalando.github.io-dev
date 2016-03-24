66ebe0c4b68a56092933f6fdb67c543e4e67b139
Tim Lossen <tim.lossen@zalando.de>
2016-03-24 16:29:12 +0100

    feat(data): cleanup js syntax, remove outdated test

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import API_CONFIG from '../constants/ApiConfig.js';
-import github from "../stores/github.js";
+import github from '../stores/github.js';
 
 var api = {};
 api.API_CONFIG = _.extend({}, API_CONFIG);
@@ -25,10 +25,11 @@ api.getRepos = function (params) {
   let options = _.merge({ limit: 15, offset: 0 }, params || {});
   let from = options.offset;
   let to = from + options.limit;
-  let withLanguage = function(x) { return params.language === 'all' || params.language === x.primaryLanguage; }
+  let withLanguage = function(x) { return params.language === 'all' || params.language === x.primaryLanguage; };
   return new Promise(function(resolve, reject) {
     resolve(_.filter(github.repos, withLanguage).slice(from, to));
-  });  
+    reject();
+  });
 };
 
 /**
@@ -38,15 +39,16 @@ api.getRepos = function (params) {
  */
 api.getLanguages = function () {
   let result = [
-    new Object({'color': '#3572a5', 'name': 'Python'}),
-    new Object({'color': '#b07219', 'name': 'Java'}),
-    new Object({'color': '#f4cf34', 'name': 'JavaScript'}),
-    new Object({'color': '#89e051', 'name': 'Shell'}),
-    new Object({'color': '#357eab', 'name': 'Go'}),
-    new Object({'color': '#db5855', 'name': 'Clojure'}),
-    new Object({'color': '#7dd3b0', 'name': 'Scala'})
+    {'color': '#3572a5', 'name': 'Python'},
+    {'color': '#b07219', 'name': 'Java'},
+    {'color': '#f4cf34', 'name': 'JavaScript'},
+    {'color': '#89e051', 'name': 'Shell'},
+    {'color': '#357eab', 'name': 'Go'},
+    {'color': '#db5855', 'name': 'Clojure'},
+    {'color': '#7dd3b0', 'name': 'Scala'}
   ];
   return new Promise(function(resolve, reject) {
     resolve(result);
-  });  
+    reject();
+  });
 };
